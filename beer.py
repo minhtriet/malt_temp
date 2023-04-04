4ab7cc8627b7c522a2e5f938494c052ae2af205b
EXT.Minh Chau <minhtriet09@gmail.com>
2023-04-04 19:02:42 +0100

    reorg code

--- beer.py
+++ beer.py
@@ -24,10 +24,12 @@ f_4 = "D(β,t) + 1/nu*( H_β*M*(β_g - β) - k_β*β)"
 input_labels = ["t"]
 output_labels = ["x", "y"]
 
-U_t = np.random.uniform(low=t_intv[0], high=t_intv[1], size=Q)
-U_s = np.random.uniform(low=s_intv[0], high=s_intv[1], size=(N, 2))
+beerPINN = BeerPINN()
 
-initial_states = U_s
+# U_t = np.random.uniform(low=t_intv[0], high=t_intv[1], size=Q)
+# U_s = np.random.uniform(low=s_intv[0], high=s_intv[1], size=(N, 2))
+
+# initial_states = U_s
 
 n_inputs = len(input_labels)
 n_outputs = len(output_labels)
@@ -56,70 +58,14 @@ n_epochs = 400_000  # Maximum number of iterations for ADAM
 lr = 1e-3  # Initial learning rate for the ADAM algorithm
 
 
-def model():
-    import numpy as np
-
-    from simulai.models import ImprovedDeepONet
-    from simulai.regression import SLFNN, ConvexDenseNetwork
-
-    n_latent = 100
-    n_inputs_b = 2
-    n_inputs_t = 1
-    n_outputs = 2
-
-    # Configuration for the fully-connected trunk network
-    trunk_config = {
-        "layers_units": 7 * [100],  # Hidden layers
-        "activations": "tanh",
-        "input_size": n_inputs_t,
-        "output_size": n_latent * n_outputs,
-        "name": "trunk_net",
-    }
-
-    # Configuration for the fully-connected branch network
-    branch_config = {
-        "layers_units": 7 * [100],  # Hidden layers
-        "activations": "tanh",
-        "input_size": n_inputs_b,
-        "output_size": n_latent * n_outputs,
-        "name": "branch_net",
-    }
-
-    # Instantiating and training the surrogate model
-    trunk_net = ConvexDenseNetwork(**trunk_config)
-    branch_net = ConvexDenseNetwork(**branch_config)
-
-    encoder_trunk = SLFNN(input_size=n_inputs_t, output_size=100, activation="tanh")
-    encoder_branch = SLFNN(input_size=n_inputs_b, output_size=100, activation="tanh")
-
-    # It prints a summary of the network features
-    trunk_net.summary()
-    branch_net.summary()
-
-    lotka_volterra_net = ImprovedDeepONet(
-        trunk_network=trunk_net,
-        branch_network=branch_net,
-        encoder_trunk=encoder_trunk,
-        encoder_branch=encoder_branch,
-        var_dim=n_outputs,
-        devices="gpu",
-        model_id="lotka_volterra_net",
-    )
-
-    return lotka_volterra_net
-
-
-lotka_volterra_net = model()
-
 residual = SymbolicOperator(
-    expressions=[f_x, f_y],
+    expressions=[f_1, f_2, f_3, f_4],
     input_vars=input_labels,
     output_vars=output_labels,
-    function=lotka_volterra_net,
-    inputs_key="input_trunk",
-    constants={"alpha": alpha, "betha": beta, "gammma": gamma, "delta": delta},
-    device="gpu",
-    engine="torch",
+    function=beerPINN.net,
+    constants={"A0_2": A0_2, "betha": beta, "gammma": gamma, "delta": delta},
+    device="cpu",
+    engine="torch"
 )
 
 # Maximum derivative magnitudes to be used as loss weights
@@ -128,7 +74,6 @@ batch_size = 10_000
 
 optimizer_config = {"lr": lr}
 
-input_data = {"input_branch": branch_input_train, "input_trunk": trunk_input_train}
 
 optimizer = Optimizer(
     "adam",
@@ -170,12 +115,273 @@ saver.write(
     template=model,
 )
 
-approximated_data = lotka_volterra_net.eval(
-    trunk_data=trunk_input_test, branch_data=branch_input_test
-)
 
-for ii in range(n_outputs):
-    plt.plot(approximated_data[:, ii], label="Approximated")
-    plt.legend()
-    plt.savefig(f"lotka_volterra_deeponet_time_int_{ii}.png")
+def test_allen_cahn(self):
+    # Our PDE
+    # Allen-cahn equation
+
+    f = "D(u, t) - mu*D(D(u, x), x) + alpha*(u**3) + beta*u"
+
+    g_u = "u"
+    g_ux = "D(u, x)"
+
+    input_labels = ["x", "t"]
+    output_labels = ["u"]
+
+    # Some fixed values
+    X_DIM = 256
+    T_DIM = 100
+
+    L = 1
+    x_0 = -1
+    T = 1
+
+
+
+    n_epochs = 50_000  # Maximum number of iterations for ADAM
+    lr = 1e-3  # Initial learning rate for the ADAM algorithm
+
+    
+
+    optimizer_config = {"lr": lr}
+
+    net = model()
+
+    residual = SymbolicOperator(
+        expressions=[f],
+        input_vars=input_labels,
+        auxiliary_expressions={"periodic_u": g_u, "periodic_du": g_ux},
+        constants={"mu": 1e-4, "alpha": 5, "beta": -5},
+        output_vars=output_labels,
+        function=net,
+        engine="torch",
+        device="gpu",
+    )
+
+    # It prints a summary of the network features
+    net.summary()
+
+    optimizer = Optimizer(
+        "adam",
+        params=optimizer_config,
+        lr_decay_scheduler_params={
+            "name": "ExponentialLR",
+            "gamma": 0.9,
+            "decay_frequency": 5_000,
+        },
+        shuffle=False,
+        summary_writer=True,
+    )
+
+    params = {
+        "residual": residual,
+        "initial_input": data_boundary_t0,
+        "initial_state": u_init,
+        "boundary_input": {
+            "periodic_u": [data_boundary_xL, data_boundary_x0],
+            "periodic_du": [data_boundary_xL, data_boundary_x0],
+        },
+        "boundary_penalties": [1, 1],
+        "weights_residual": [1],
+        "initial_penalty": 100,
+    }
+
+    optimizer.fit(
+        op=net,
+        input_data=data,
+        n_epochs=n_epochs,
+        loss="pirmse",
+        params=params,
+        device="gpu",
+    )
+
+    saver = SPFile(compact=False)
+    saver.write(save_dir="/tmp", name="allen_cahn_net", model=net, template=model)
+
+    # Evaluation and post-processing
+    X_DIM_F = 5 * X_DIM
+    T_DIM_F = 5 * T_DIM
+
+    x_f = np.linspace(*x_interval, X_DIM_F)
+    t_f = np.linspace(*t_interval, T_DIM_F)
+
+    T_f, X_f = np.meshgrid(t_f, x_f, indexing="ij")
+
+    data_f = np.hstack([X_f.flatten()[:, None], T_f.flatten()[:, None]])
+
+    # Evaluation in training dataset
+    approximated_data = net.cpu().eval(input_data=data_f)
+
+    U_f = approximated_data.reshape(T_DIM_F, X_DIM_F)
+
+    fig, ax = plt.subplots()
+    ax.set_aspect("auto")
+    gf = ax.pcolormesh(X_f, T_f, U_f, cmap="jet")
+    fig.colorbar(gf)
+
+    plt.savefig("allen_cahn.png")
+
+def test_allen_cahn_rbf(self):
+    # Our PDE
+    # Allen-Cahn equation
+
+    f = "D(u, t) - mu*D(D(u, x), x) + alpha*(u**3) + beta*u"
+
+    g_u = "u"
+    g_ux = "D(u, x)"
+
+    input_labels = ["x", "t"]
+    output_labels = ["u"]
+
+    n_inputs = len(input_labels)
+    n_outputs = len(output_labels)
+
+    # Some fixed values
+    X_DIM = 100
+    T_DIM = 20
+    Nk = 50
+    L = 1
+    x_0 = -1
+    T = 1
+
+    # Generating the training grid
+
+    x_interval = [x_0, L]
+    t_interval = [0, T]
+
+    intervals = [x_interval, t_interval]
+
+    intv_array = np.vstack(intervals).T
+
+    # Regular grid
+    x_0, x_L = x_interval
+    t_0, t_L = t_interval
+    dx = (x_L - x_0) / X_DIM
+    dt = (t_L - t_0) / T_DIM
+
+    grid = np.mgrid[t_0 + dt : t_L + dt : dt, x_0:x_L:dx]
+
+    data = np.hstack([grid[1].flatten()[:, None], grid[0].flatten()[:, None]])
+
+    data_init = np.linspace(*x_interval, X_DIM)
+    u_init = (data_init**2) * np.cos(np.pi * data_init)[:, None]
+
+    # Boundary grids
+    data_boundary_x0 = np.hstack(
+        [
+            x_interval[0] * np.ones((T_DIM, 1)),
+            np.linspace(*t_interval, T_DIM)[:, None],
+        ]
+    )
+
+    data_boundary_xL = np.hstack(
+        [
+            x_interval[-1] * np.ones((T_DIM, 1)),
+            np.linspace(*t_interval, T_DIM)[:, None],
+        ]
+    )
+
+    data_boundary_t0 = np.hstack(
+        [
+            np.linspace(*x_interval, X_DIM)[:, None],
+            t_interval[0] * np.ones((X_DIM, 1)),
+        ]
+    )
+
+    # Visualizing the training mesh
+    plt.scatter(*np.split(data, 2, axis=1))
+    plt.scatter(*np.split(data_boundary_x0, 2, axis=1))
+    plt.scatter(*np.split(data_boundary_xL, 2, axis=1))
+    plt.scatter(*np.split(data_boundary_t0, 2, axis=1))
+
+    plt.show()
+    plt.close()
+
+    n_epochs = 100  # _000  # Maximum number of iterations for ADAM
+    lr = 1e-3  # Initial learning rate for the ADAM algorithm
+
+    # Configuration for the fully-connected network
+    config = {
+        "layers_units": [128, 128],
+        "activations": "tanh",
+        "input_size": 1,
+        "output_size": Nk,
+        "name": "allen_cahn_net",
+    }
+
+    optimizer_config = {"lr": lr}
+
+    # Instantiating and training the surrogate model
+    coeff_net = DenseNetwork(**config)
+    net = ModalRBFNetwork(
+        xmin=-1,
+        xmax=1,
+        Nk=Nk,
+        var_dim=1,
+        Sigma=0.5,
+        coeff_network=coeff_net,
+        name="allen_cahn_net",
+    )
+
+    residual = SymbolicOperator(
+        expressions=[f],
+        input_vars=input_labels,
+        auxiliary_expressions={"periodic_u": g_u, "periodic_du": g_ux},
+        constants={"mu": 1e-4, "alpha": 5, "beta": -5},
+        output_vars=output_labels,
+        function=net,
+        engine="torch",
+    )
+
+    # It prints a summary of the network features
+    net.summary()
+
+    # optimizer = Optimizer('bbi', params=optimizer_config)
+    optimizer = Optimizer(
+        "adam",
+        params=optimizer_config,
+        lr_decay_scheduler_params={
+            "name": "ExponentialLR",
+            "gamma": 0.9,
+            "decay_frequency": 5_000,
+        },
+    )
+
+    params = {
+        "residual": residual,
+        "initial_input": data_boundary_t0,
+        "initial_state": u_init,
+        "boundary_input": {
+            "periodic_u": [data_boundary_xL, data_boundary_x0],
+            "periodic_du": [data_boundary_xL, data_boundary_x0],
+        },
+        "boundary_penalties": [1, 1],
+        "initial_penalty": 100,
+    }
+
+    optimizer.fit(
+        op=net, input_data=data, n_epochs=n_epochs, loss="pirmse", params=params
+    )
+
+    # Evaluation and post-processing
+    X_DIM_F = 5 * X_DIM
+    T_DIM_F = 5 * T_DIM
+
+    x_f = np.linspace(*x_interval, X_DIM_F)
+    t_f = np.linspace(*t_interval, T_DIM_F)
+
+    T_f, X_f = np.meshgrid(t_f, x_f, indexing="ij")
+
+    data_f = np.hstack([X_f.flatten()[:, None], T_f.flatten()[:, None]])
+
+    # Evaluation in training dataset
+    approximated_data = net.eval(input_data=data_f)
+
+    U_f = approximated_data.reshape(T_DIM_F, X_DIM_F)
+
+    fig, ax = plt.subplots()
+    ax.set_aspect("auto")
+    gf = ax.pcolormesh(X_f, T_f, U_f, cmap="jet")
+    fig.colorbar(gf)
+
     plt.show()
