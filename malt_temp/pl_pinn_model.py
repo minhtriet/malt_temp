5030fdf7baf94bc47856bf42690ebc04069dd030
EXT.Minh Chau <minhtriet09@gmail.com>
2023-05-03 22:34:53 +0100

    add lightning training model

---
+++ malt_temp/pl_pinn_model.py
@@ -0,0 +1,28 @@
+import lightning.pytorch as pl
+import torch
+import torch.functional as F
+
+
+class PLPinnModule(pl.LightningModule):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def training_step(self, batch, batch_idx):
+        # train loop
+        x, y = batch
+        prediction = self.model(x)
+        loss = F.mse_loss(prediction, x)
+        self.log("train_loss", loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        # this is the test loop
+        x, y = batch
+        prediction = self.model(x)
+        test_loss = F.mse_loss(prediction, x)
+        self.log("test_loss", test_loss)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
