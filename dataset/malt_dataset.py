d061b4242c60f19c2ff33f1426a6c948bc7771ca
EXT.Minh Chau <minhtriet09@gmail.com>
2023-05-05 23:44:39 +0100

    force return alpha related things

--- dataset/malt_dataset.py
+++ dataset/malt_dataset.py
@@ -1,21 +1,29 @@
-import pandas as pd
+import torch
 from torch.utils.data import Dataset
 
 
 class MaltDataset(Dataset):
-    def __init__(self, filepath, len_input=3, len_output=3):
-        df = pd.read_csv(filepath)
-        # input
-        self.time, self.temp = df['Time'].values, df['Temperature'].values
-        # output
-        self.alpha_amilase = df['AlfaAmilase'].values
-        self.grain_alpha_amilase = df['AlfaAmilase_Grain'].values   # [U/l]
+    def __init__(self, df, len_input):
+        """
+        :param filepath: A df that contains the columns
+            Time,Temperature,AlfaAmilase,AlfaAmilase_Grain,BetaAmilase,BetaAmilase_grain,
+            Starch,Dextrins,Glucose,Maltose,Maltotriose,Limit_Dextrins,SolidosFermentaveis,
+            SolidosNaoFermentaveis,SolidosTotais,PercFermentaveis,Extrato,MashingEfficiency,Dp1,Dp2,Dp3,Dp4Plus
+        :param len_input: Length of each slice of the dataset to feed to the nn
+        """
         self.len_input = len_input
-        self.len_output = len_output
-        # todo run the code on windows machine and see input, output
+        input_columns = ['Time', 'Temperature']
+        # input
+        self.input = torch.Tensor(df[input_columns].values)
+        # output
+        df_output = df.drop(input_columns, axis=1)
+        self.output = torch.Tensor(df_output.values)
+        self.output_to_index = dict(zip(df_output.columns, range(len(df_output.columns))))
 
     def __len__(self):
-        return len(self.time)
+        return self.input.shape[0]-self.len_input
 
     def __getitem__(self, idx):
-        return idx, self.temp[idx], self.alpha_amilase[idx], self.grain_alpha_amilase[idx]
+        return self.input[idx:idx+self.len_input], \
+            self.output[idx:idx+self.len_input][[self.output_to_index['BetaAmilase'],
+                                                 self.output_to_index['BetaAmilase_grain']]]
