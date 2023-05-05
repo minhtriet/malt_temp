367d950de6ac3737f072e17f19c9d6372fa3dbe0
EXT.Minh Chau <minhtriet09@gmail.com>
2023-05-05 23:41:53 +0100

    add pl instead of loop

--- run.py
+++ run.py
@@ -1,63 +1,44 @@
 from pathlib import PurePath
 
+import lightning.pytorch as pl
 import pandas as pd
 import torch
-from torch.nn import MSELoss
-from torch.optim import Adam
 from torch.utils.data import DataLoader
 
 import config
 from dataset import malt_dataset
-from malt_temp import model, initial_condition
+from malt_temp import model, initial_condition, pl_pinn_model
 
 path_input = PurePath("dataset", "Mash_Data.csv")
 mash_data = pd.read_csv(path_input)
 
-ic = initial_condition.InitialCondition(
-    time=mash_data.iloc[0]['Time'],
-    temperature=mash_data.iloc[0]['Temperature'],
-    alpha_adjust=initial_condition.InitialAlfa_Adjust,
-    grain_alpha_adjust=initial_condition.InitialGrainAlpha_Adjust,
-    beta_adjust=initial_condition.InitialBeta_Adjust,
-    grain_beta_adjust=initial_condition.InitialGrainBeta_Adjust
-)
-
-dataset = malt_dataset.MaltDataset(path_input, len_input=config.INPUT_LENGTH)
+dataset = malt_dataset.MaltDataset(mash_data.tail(len(mash_data)-1), len_input=config.INPUT_LENGTH)
 dataloader = DataLoader(dataset)
 
-
-ic = initial_condition.InitialCondition(
-    time=mash_data.iloc[0]['Time'],
-    temperature=mash_data.iloc[0]['Temperature'],
-    alpha_adjust=initial_condition.InitialAlfa_Adjust,
-    grain_alpha_adjust=initial_condition.InitialGrainAlpha_Adjust,
-    beta_adjust=initial_condition.InitialBeta_Adjust,
-    grain_beta_adjust=initial_condition.InitialGrainBeta_Adjust
-)
 
 if torch.backends.mps.is_available():
     device = torch.device("mps")
 else:
     device = torch.device("cpu")
 
-initial_cond = initial_condition.InitialCondition(alpha_adjust=initial_condition.InitialAlfa_Adjust,
+initial_cond = initial_condition.InitialCondition(time=mash_data.iloc[0]['Time'],
+                                                  temperature=mash_data.iloc[0]['Temperature'],
+                                                  beta=mash_data.iloc[0]['BetaAmilase'],
+                                                  grain_beta=mash_data.iloc[0]['BetaAmilase_grain'],
+                                                  alpha_adjust=initial_condition.InitialAlfa_Adjust,
                                                   grain_alpha_adjust=initial_condition.InitialGrainAlpha_Adjust,
                                                   beta_adjust=initial_condition.InitialBeta_Adjust,
-                                                  grain_beta_adjust=initial_condition.InitialGrainBeta_Adjust,
-                                                  starch_adjust=initial_condition.InitialStarch_Adjust,
-                                                  dextrins_adjust=initial_condition.InitialDextrins_Adjust)
+                                                  grain_beta_adjust=initial_condition.InitialGrainBeta_Adjust)
 
-net = model.PINN_Model(len_input=config.INPUT_LENGTH, nodes=22, layers=1, y0=initial_cond)
-criterion = MSELoss()
-optimizer = Adam(net.parameters(), lr=0.01)
-net.train()
-for idx, data in enumerate(dataloader):
-    time, temp, y = data
-    optimizer.zero_grad()
-    out = net(time)
-    loss_data = criterion(y, out)
-    loss_physic = 7
-    optimizer.step()
+pinn_model = model.PINN_Model(nodes=4, layers=1, y0=initial_cond)
+
+pl.seed_everything(1234, workers=True)
+
+pl_model = pl_pinn_model.PLPinnModule(pinn_model)
+trainer = pl.Trainer()
+
+# train the model
+trainer.fit(pl_model, train_dataloaders=DataLoader(dataset))   # todo add val_loader
 
 # todo jacobian tricks
 # Their net production rates are much lower than their
