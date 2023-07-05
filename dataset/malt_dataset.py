37c17e40792fcfe071d27257a2889983f08443b7
minhtriet <minhtriet09@gmail.com>
2023-07-05 17:08:13 +0700

    format dataset output

--- dataset/malt_dataset.py
+++ dataset/malt_dataset.py
@@ -1,21 +1,28 @@
+import logging
+
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
 
 
 class MaltDataset(Dataset):
-    def __init__(self, filepath, len_input=3, len_output=3):
+    """A dataset that contains the envolvement of the concentration of the malt sacharization
+    temperature is a constant till now
+    """
+    def __init__(self, filepath):
         df = pd.read_csv(filepath)
         # input
         self.time, self.temp = df['Time'].values, df['Temperature'].values
         # output
-        self.alpha_amilase = df['AlfaAmilase'].values
-        self.grain_alpha_amilase = df['AlfaAmilase_Grain'].values   # [U/l]
-        self.len_input = len_input
-        self.len_output = len_output
-        # todo run the code on windows machine and see input, output
+        df = df[['AlfaAmilase', 'AlfaAmilase_Grain']]
+        self.features_index = {column_name: df.columns.get_loc(column_name) for column_name in df.columns}
+        logging.info(f"Features are {self.features_index}")
+        # todo scale
+        self.y = torch.from_numpy(df.values)
 
     def __len__(self):
-        return len(self.time)
+        return len(self.time)-1
 
     def __getitem__(self, idx):
-        return idx, self.temp[idx], self.alpha_amilase[idx], self.grain_alpha_amilase[idx]
+        return {'x': torch.FloatTensor([idx, self.temp[idx]]),
+                'y': self.y[idx]}
